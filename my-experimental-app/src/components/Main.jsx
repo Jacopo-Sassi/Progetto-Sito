a83679b60ec6e1a00f5f9e2aaccb2677550cc6aa
Jacopo Sassi <jacopo.sassi@intesys.it>
2025-07-15 11:13:40 +0200

    added button to main

--- my-experimental-app/src/components/Main.jsx
+++ my-experimental-app/src/components/Main.jsx
@@ -4,7 +4,8 @@ function Main() {
     <div className="main">
         <div className="left-container-main">
             <h1>Let your dreams become true</h1>
-            <p>Chose your destination with us</p>
+            <p>Take a moment to relax</p>
+            <button className="btn-title">Explore Now</button>
         </div>
         <div className="right-container-main">
             <img className="img-main-title" src="https://imgs.search.brave.com/ttIts1tzlOf-eUvma7t0tFQasqbFo-sGCNHvv2mBR9g/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zaXZp/YWdnaWEuaXQvd3At/Y29udGVudC91cGxv/YWRzL3NpdGVzLzIv/MjAyNC8xMi9CZXF1/aWEuanBn" alt="Travel" />
--- my-experimental-app/src/components/Section.jsx
+++ my-experimental-app/src/components/Section.jsx
@@ -3,7 +3,7 @@ import CardHero from "./CardHero"
 function Section (){
     return (
         <div className="section">
-            <h2>Section Title</h2>
+            <h2 className="section-title">Our Destinations</h2>
             <div className="card-container">
                 <CardHero />
                 <CardHero />
