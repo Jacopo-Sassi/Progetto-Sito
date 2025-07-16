95568898ae027f745d40a1a8ad6aa8ee0ef3dd7d
Jacopo Sassi <sassijacopp@gmail.com>
2025-07-16 12:22:02 +0200

    fixed distance between cards

--- my-experimental-app/src/components/CardHero.jsx
+++ my-experimental-app/src/components/CardHero.jsx
@@ -1,29 +1,39 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-function CardHero({ 
+function CardHero({
   imageUrl = "https://via.placeholder.com/300", // Valore di default
-  title = "Titolo Default", 
+  title = "Titolo Default",
   text = "Descrizione default della card",
-  buttonText = "Scopri di più" 
+  buttonText = "Scopri di più",
 }) {
   return (
-    <div className='d-flex justify-content-center align-items-center' style={{ height: '40vh' }}>
-      <Card className="card-hero-animated card-entrance" style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={imageUrl} style={{ 
-          height: '200px',
-          objectFit: 'cover' ,
-          transition: 'transform 0.3s ease'
-        }}  />
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "50vh" }}
+    >
+      <Card
+        className="card-hero-animated card-entrance"
+        style={{ width: "18rem" }}
+      >
+        <Card.Img
+          variant="top"
+          src={imageUrl}
+          style={{
+            height: "200px",
+            objectFit: "cover",
+            transition: "transform 0.3s ease",
+          }}
+        />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{text}</Card.Text>
-          <Button className="pulse-on-hover" variant="primary">{buttonText} </Button>
+          <Button className="pulse-on-hover" variant="primary">
+            {buttonText}{" "}
+          </Button>
         </Card.Body>
       </Card>
-      
     </div>
-    
   );
 }
 
